4b369276c115e0f9b978f1561a03b4e78a983922
William Velazquez <william.velazquez.a@gmail.com>
2018-07-08 17:42:56 -0500

    Paddle Movement with Mouse Position

---
+++ Pong/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Input.mousePosition);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z);
+	}
+}
