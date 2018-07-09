700f20562f2f7aa6f5e5fafdfeaafc8e9a920588
William Velazquez <william.velazquez.a@gmail.com>
2018-07-08 19:09:26 -0500

    Ball Movement and Game Started Validation

---
+++ Pong/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour {
+
+    public Transform paddle;
+    bool gameStarted = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!gameStarted)
+        {
+            float posDif = paddle.position.x - transform.position.x;
+            transform.position = new Vector3(paddle.position.x - posDif, paddle.position.y, paddle.position.z);
+
+            //Left Clic=0
+            if (Input.GetMouseButtonDown(0))
+            {
+                gameStarted = true;
+            }
+        }
+        else
+        {
+
+        }
+        
+	}
+}
