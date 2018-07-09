d3375bb0be40a9ade54ea0acbe7fa8460f7df91e
William Velazquez <william.velazquez.a@gmail.com>
2018-07-09 00:15:41 -0500

    Point Detection and Visualization

--- Pong/Assets/Scripts/DeadZone.cs
+++ Pong/Assets/Scripts/DeadZone.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class DeadZone : MonoBehaviour {
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Colisión");
-    }
+    public Text playerScoreText;
+    public Text enemyScoreText;
+
+    int playerScoreQuantity;
+    int enemyScoreQuantity;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Trigger");
+        if(gameObject.tag == "Left")
+        {
+            enemyScoreQuantity++;
+            UpdateScoreLabel(enemyScoreText,enemyScoreQuantity);
+        }
+        else if (gameObject.CompareTag("Right"))
+        {
+            playerScoreQuantity++;
+            UpdateScoreLabel(playerScoreText, playerScoreQuantity);
+        }
+    }
+
+    void UpdateScoreLabel( Text label, int score)
+    {
+        label.text = score.ToString();
     }
 }
